7680c8c3cdd1983187a5d6b8335dc2a8730c4158
HenriqueMafort <hmafort@gmail.com>
2023-06-09 10:11:42 -0300

    Inserindo novo modulo

--- Modulo3/Consultas/Data/ApplicationContext.cs
+++ Modulo3/Consultas/Data/ApplicationContext.cs
@@ -7,15 +7,21 @@ namespace Curso.Data
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<Departamento> Departamentos {get;set;}
-        public DbSet<Funcionario> Funcionarios {get;set;}
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            const string srtConnection="Server=localhost; Database=ConsultasDB; Trusted_Connection=True; TrustServerCertificate=True;";
+            const string srtConnection = "Server=localhost; Database=ConsultasDB; Trusted_Connection=True; TrustServerCertificate=True;";
+            //optionsBuilder.UseSqlServer(srtConnection, p => p.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery))
             optionsBuilder.UseSqlServer(srtConnection)
             .EnableSensitiveDataLogging()
             .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // basicamente estou sobreescrevendo o onmodelcreateing do departamento e dizendo para ele trazer somente os empregados que forem diferentes de true
+        {
+            // modelBuilder.Entity<Departamento>().HasQueryFilter(p => !p.Excluido);//o hasqueryfilter faz um filtro e tras somente oque tiver dentro do parametro. no caso seria tudo oque for diferente de true Excluido 
         }
     }
 
